8a8f8858236993a37398a26900a6850f001d99d5
Kyle Bell <bellkyle97@gmail.com>
2018-03-31 09:09:27 -0500

    map

--- AwesomeSauce/app/src/main/java/com/example/kyle/awesomesauce/TabActivity.java
+++ AwesomeSauce/app/src/main/java/com/example/kyle/awesomesauce/TabActivity.java
@@ -1,11 +1,20 @@
 package com.example.kyle.awesomesauce;
 
+        import android.content.DialogInterface;
+        import android.support.v7.app.AlertDialog;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.text.InputType;
         import android.view.Menu;
         import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.EditText;
         import android.widget.ListView;
         import android.widget.TabHost;
+        import android.widget.TextView;
+
+        import com.google.android.gms.maps.MapView;
+        import com.google.android.gms.maps.SupportMapFragment;
 
 public class TabActivity extends AppCompatActivity {
     TabHost tabHost;
@@ -48,8 +57,42 @@ public class TabActivity extends AppCompatActivity {
         CheckListAdapter adapter = new CheckListAdapter(this, checklist);
         listView = (ListView) findViewById(R.id.check_list_view);
         listView.setAdapter(adapter);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(new MapsActivity());
     }
 
+    String m_Text;
+    public void updateDestinationButton(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Title");
 
-    
+// Set up the input
+        final EditText input = new EditText(this);
+        final EditText input2 = new EditText(this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        //input2.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        //builder.setView(input2);
+
+// Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+                TextView t = (TextView) findViewById(R.id.destination);
+                t.setText(m_Text);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
 }
